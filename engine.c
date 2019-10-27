9fdaf52f80a95ea0b52a66c1fc1c262d018057d7
Julien2000info <julien.jacquemin2000@hotmail.fr>
2019-10-27 18:08:01 +0100

    Réalise les actions demandée par le joueur

---
+++ engine.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Engine.h"
+
+/*Moteur du jeu*/
+
+int play(int width, int height,int nbrBombs,
+         Move (*computeMove)(Grid *), int show)
+{
+    int WonOrLost = 0; //Détermine si la partie est gagnée ou perdue.
+    Grid *mine_field = gridInit(width, height, nbrBombs);
+    Move action;
+
+    while (!WonOrLost){ //Tant qu'on a pas perdu ou gagné, on continue!
+
+        if (show)
+            gridPrint(mine_field);
+        action = computeMove(mine_field);
+        if (action.flag == 0){ //On révèle
+            if (gridReveal(mine_field, action.x, action.y) == 1)//gridreveal retourne 1 si la case est une bombe => PERDDU => sort de la boucle.
+                break;
+        }
+        else if (action.flag == 1) //Pose un flag
+            gridSetFlag(mine_field, action.x, action.y);
+        else if(action.flag == -1){ //Abandon
+            printf("Thanks for playing with us!");
+            return 0;
+        }
+        else{ //Commande non connue, on recommence la boucle sans afficher la grille.
+            action.flag = 2;
+            printf("Unknown action.\n");
+            continue;
+        }
+        WonOrLost = gridWon(mine_field);
+
+    }
+    gridPrint(mine_field); //Quelque soit la valeur de show, on affiche la grille de fin!
+    gridFree(mine_field);
+
+    return WonOrLost;
+}
