beb12e8a17798b2ac4204bde1951ad77eb8ee294
louissdeniss <52701182+louissdeniss@users.noreply.github.com>
2019-10-28 20:17:43 +0100

    Update solver.c
    programmation défensive (petite récursive qui passe bueno)

--- solver.c
+++ solver.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include "solver.h"
 
+static int verifyAppartenance(Grid *grid, int x, int y);
+
 Move human(Grid *grid){
     Move action;
 
@@ -13,6 +15,38 @@ Move human(Grid *grid){
 
     printf("Action: ");
     scanf("%d", &(action.flag));
+    
+    //MODIF LOUIS
+    //gestion du cas où les coordonnées entrées n'appartiennent pas au tableau
+    if(!verifyAppartenance(grid, action.x, action.y)){
+        printf("Veuillez entrer des coordonnées valables. \n");
+        return human(grid);
+    }
+
+    //gestion du cas où on veut révéler une case qui l'est déjà
+    if(gridIsExplored(grid, action.x, action.y) && !action.flag){
+        printf("Cette case a déjà été révélée! \n");
+        return human(grid);
+    }
+
+    //gestion du cas où l'action demandée n'existe pas
+    if(action.flag < -1 || action.flag > 1){
+        printf("Veuillez entrer une action existante. \n");
+        return human(grid);
+    }
 
     return action;
+}
+
+//fonction qui vérifie si la case de cooordonnées x,y appartient bien à la grille
+static int verifyAppartenance(Grid *grid, int x, int y){
+    int width=gridWidth(grid);
+    int height=gridHeight(grid);
+    if(x >= 0 && x < width && y >= 0 && y < height)
+        return 1;
+    else
+    {
+        return 0;
+    }
+    
 }
